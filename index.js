4f87b69af780f2c8cf41f18f33d251ac5e7ab678
Sriiindarti <sriiindarti03@gmail.com>
2024-02-15 11:07:01 +0700

    Belajar Membuat Local Module

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
-require('./coba2'); //contoh memanggil file coba2, sebelum menjalankan code di file index.js
+//Core Module
+// const fs = require('fs')
 
-// yang akan jalan hanya index.js
-// sebagai file utama js
+// require('./coba2'); //contoh memanggil file coba2, sebelum menjalankan code di file index.js (Local Module)
 
-const hai = 'halo ini yang jalan adalah index';
-console.log(hai);
+// // yang akan jalan hanya index.js
+// // sebagai file utama js
 
-// tapi kita bisa manggil file js lainnya
-// dengan cara menambahkan "require"
+// const hai = 'halo ini yang jalan adalah index';
+// console.log(hai);
 
-require('./coba'); //contoh memanggil file coba, setelah menjalankan code di file index.js
+// // tapi kita bisa manggil file js lainnya
+// // dengan cara menambahkan "require"
+
+// require('./coba'); //contoh memanggil file coba, setelah menjalankan code di file index.js (Local Module)
 
 
-// case selanjutnya mau menjalankan variabel cetak nama yang ada di panggilvar.js untuk dijalankan di index.js
+// // case selanjutnya mau menjalankan variabel cetak nama yang ada di panggilvar.js untuk dijalankan di index.js
 
-// caranya :
-// 1. menuliskan code export terlebih dahulu di dalam file panggilvar.js
-// 2. simpan hasil tangkapan dari panggil.var ke dalam sebuah variabel ("const hasilTangkapanVariabel")
+// // caranya :
+// // 1. menuliskan code export terlebih dahulu di dalam file panggilvar.js
+// // 2. simpan hasil tangkapan dari panggil.var ke dalam sebuah variabel ("const hasilTangkapanVariabel")
 
-const hasilTangkapanVariabel = require('./panggilVar');
+// const hasilTangkapanVariabel = require('./panggilVar'); //(Local Module)
 
-console.log(hasilTangkapanVariabel ('Indarti Sri'));
+// //third Part Modules / NPM Module / Node Modules
+// const moment = require('moment');
+
+// console.log(hasilTangkapanVariabel ('Indarti Sri'));
+
+
+// Case selanjutnya kalo mau manggil beberapa fungsi dari file lain
+// caranya
+// 1. file dikirim dari pangglvar berupa properti dan methode
+// 2. selanjutnya panggil
+
+const panggilVar = require('./panggilVar');
+
+console.log(panggilVar.metCetakNama('Sri Cantik'), panggilVar.propPI, panggilVar.objMahasiswa.cetakMhs(), new panggilVar.Orang());
--- panggilVar.js
+++ panggilVar.js
@@ -5,6 +5,40 @@ function cetakNama(nama){
       return `Halo, nama saya ${nama}`;
 }
 
+const PI = 3.14;
+
+const mahasiswa = {
+      nama : 'Doddy Ferdiansyah',
+      umur : 20,
+
+      cetakMhs() {
+            return `Halo,nama saya ${this.nama} dan saya ${this.umur} tahun.`;
+      }
+}
+
+class Orang {
+     constructor(){
+      console.log('Objek orang telah dibuat')
+     } 
+}
+
+
+
+
 // export module agar variable cetak nama dapat diakses di index.js
-module.exports = cetakNama;
+// module.exports = cetakNama;
+
+//Case Selanjutnya
+// module.exports.metCetakNama = cetakNama;
+// module.exports.propPI = PI;
+// module.exports.objMahasiswa = mahasiswa;
+// module.exports.Orang = Orang;
+
+module.exports = {
+      metCetakNama: cetakNama,
+      propPI:  PI,
+      objMahasiswa: mahasiswa,
+      Orang: Orang,
+}
+
 
